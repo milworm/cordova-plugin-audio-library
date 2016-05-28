f6fa680077928e6feb4937f09417d223a66dff7d
Marcel Goya <marcel.goya@icloud.com>
2016-05-28 02:39:32 +0100

    Android 23> permission update

--- src/android/AudioLibrary.java
+++ src/android/AudioLibrary.java
@@ -7,14 +7,21 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.Manifest;
+import android.os.Build;
 import android.provider.MediaStore;
 import android.database.Cursor;
 import android.net.Uri;
+import android.util.Log;
 
 /**
  * This class echoes a string called from JavaScript.
  */
-public class AudioLibrary extends CordovaPlugin {
+public class AudioLibrary extends CordovaPlugin  {
+
+    private CallbackContext callbackContext = null;
+
+    private String message;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -28,6 +35,24 @@ public class AudioLibrary extends CordovaPlugin {
     }
 
     private void getItems(String message, CallbackContext callbackContext) {
+
+        this.callbackContext = callbackContext;
+        this.message = message;
+
+        // Request permission during run-time
+        if (Build.VERSION.SDK_INT >= 23) {
+            if(!this.cordova.hasPermission(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                this.cordova.requestPermission(this, 0, Manifest.permission.READ_EXTERNAL_STORAGE);
+            } else {
+                this.getList();
+            }
+        } else {
+            this.getList();
+        }
+    }
+
+    private void getList() {
+
         final String sortOrder = MediaStore.Audio.AudioColumns.TITLE + " COLLATE LOCALIZED ASC";
 
         Cursor cursor = null;
@@ -48,34 +73,35 @@ public class AudioLibrary extends CordovaPlugin {
 
             if (cursor != null) {
                 cursor.moveToFirst();
-
                 while (!cursor.isAfterLast()) {
-                    String title = cursor.getString(0);
-                    String artist = cursor.getString(1);
-                    String path = cursor.getString(2);
-                    String songDuration = cursor.getString(4);
-
-                    JSONObject item = new JSONObject();
-
-                    item.put("title", cursor.getString(0));
-                    item.put("artist", cursor.getString(1));
-                    item.put("path", cursor.getString(2));
-                    item.put("duration", cursor.getString(4));
-                    item.put("id", cursor.getString(5));
-
-                    result.put(item);
-
+                    if(cursor.getColumnCount() == 5) {
+                        JSONObject item = new JSONObject();
+                        item.put("title", cursor.getString(0));
+                        item.put("artist", cursor.getString(1));
+                        item.put("path", cursor.getString(2));
+                        item.put("duration", cursor.getString(3));
+                        item.put("_id", cursor.getString(4));
+                        result.put(item);
+                    }
                     cursor.moveToNext();
                 }
             }
         } catch (Exception e) {
-            callbackContext.error(e.toString());
+            this.callbackContext.error(e.toString());
         } finally {
             if (cursor != null) {
                 cursor.close();
             }
         }
 
-        callbackContext.success(result.toString());
+        this.callbackContext.success(result.toString());
+    }
+
+    public void onRequestPermissionResult(int requestCode, String[] permissions,
+                                          int[] grantResults) throws JSONException
+    {
+        if(grantResults[0] == 0) {
+            this.getItems(this.message, this.callbackContext);
+        }
     }
 }
